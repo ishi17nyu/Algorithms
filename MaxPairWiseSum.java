18ce02f67c366895d995cd31c93370937dd0471a
ishi17nyu <iv447@nyu.edu>
2018-09-26 14:34:53 -0400

    Added pair wise sum  code for Algorithms Course

---
+++ MaxPairWiseSum.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+import java.util.Arrays;
+
+public class MaxPairWiseSum {
+
+	static long getMaxPairwiseProduct(long[] numbers) {
+        int n = numbers.length;
+        Arrays.sort(numbers);
+        return numbers[n-1] * numbers[n-2];
+    }
+ 
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        long[] numbers = new long[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(
+        		getMaxPairwiseProduct(numbers)
+      		);
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
